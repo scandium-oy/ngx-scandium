ffcf2a2f9ac962d0e7056baf31b4113324c43323
Markus Montonen <montonen.markus@gmail.com>
2022-12-09 10:22:57 +0200

    Project

---
+++ projects/ngx-scandium/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-scandium
+ */
+
+export * from './lib/directives/format-number/format-number.directive';
+export * from './lib/directives/format-number/format-number.module';
