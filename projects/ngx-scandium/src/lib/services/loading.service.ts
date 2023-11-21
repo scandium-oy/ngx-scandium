be0409d9fda4c90d4be869d34b5a29c60fcd8659
Markus Montonen <montonen.markus@gmail.com>
2023-11-21 11:11:06 +0200

    Loading service update

--- projects/ngx-scandium/src/lib/services/loading.service.ts
+++ projects/ngx-scandium/src/lib/services/loading.service.ts
@@ -1,25 +1,39 @@
 import { Injectable } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoadingService {
 
-  constructor(private loadingCtrl: LoadingController) { }
+  private loading: HTMLIonLoadingElement | null = null;
+
+  constructor(
+    private loadingCtrl: LoadingController,
+    private translate: TranslateService,
+  ) { }
 
   public showLoading(duration?: number) {
-    this.loadingCtrl.create({
-      spinner: 'circles',
-      duration,
-    }).then((loading) => {
-      loading.present();
-    });
+    if (this.loading == null) {
+      this.loadingCtrl.create({
+        spinner: 'circles',
+        duration,
+        message: this.translate.instant('general.pleaseWait'),
+        keyboardClose: false,
+        translucent: true,
+      }).then((loading) => {
+        this.loading = loading;
+        this.loading.present();
+        console.log('Showing loading');
+      });
+    }
   }
 
   public hideLoading() {
-    try {
-      this.loadingCtrl.dismiss();
-    } catch (e) { }
+    this.loading?.dismiss().then(() => {
+      console.log('Hiding loading');
+      this.loading = null;
+    });
   }
 }
