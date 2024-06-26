202e8d7b174a35a5b354938ad3acf1e1de654c40
Markus Montonen <montonen.markus@gmail.com>
2024-06-26 08:28:29 +0300

    Disable camera

--- projects/ngx-scandium/src/lib/components/camera-button/camera-button.component.ts
+++ projects/ngx-scandium/src/lib/components/camera-button/camera-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, input } from '@angular/core';
 import { FileUpload } from '../../models';
 import { CameraService } from '../../services/camera.service';
 import { LoadingService } from '../../services/loading.service';
@@ -14,6 +14,8 @@ export class CameraButtonComponent {
 
   @Input()
   color = 'primary';
+
+  disabled = input(false);
 
   @Input()
   name: string = '';
