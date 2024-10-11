b22d7eb7a1e51954679c123a7b5bb7a997561c4c
Markus Montonen <montonen.markus@gmail.com>
2024-10-11 14:27:12 +0300

    Logo width

--- projects/ngx-scandium/src/lib/components/logo/logo.component.ts
+++ projects/ngx-scandium/src/lib/components/logo/logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
@@ -7,6 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class LogoComponent {
 
-  @Input()
-  logo: string = '';
+  logo = input.required<string>();
 }
