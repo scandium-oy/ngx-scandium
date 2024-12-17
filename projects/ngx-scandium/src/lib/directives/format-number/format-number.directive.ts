2a7c6032cbb0cf7eb7a77a91f61c646d58b0c151
Markus Montonen <montonen.markus@gmail.com>
2024-12-17 09:22:02 +0200

    Version

--- projects/ngx-scandium/src/lib/directives/format-number/format-number.directive.ts
+++ projects/ngx-scandium/src/lib/directives/format-number/format-number.directive.ts
@@ -4,14 +4,16 @@ import { Directive, ElementRef, Input } from '@angular/core';
 export class FormatNumberDirective {
 
   @Input()
-  set appFormatNumber(value: any) {
+  set formatNumber(value: number | string) {
     this.trimValue(value);
   }
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {
 
-  private trimValue(value: any) {
-    const replaced = typeof value === 'number' ? value.toFixed(2) : value;
+  }
+
+  private trimValue(value: number | string) {
+    const replaced = typeof value === 'number' ? Number.parseFloat(value.toFixed(2)) : value;
     this.el.nativeElement.innerHTML = replaced;
   }
 }
