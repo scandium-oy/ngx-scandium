8c6b708632aeaa31585f5bbe23e36ecfcd878263
Markus Montonen <montonen.markus@gmail.com>
2026-02-23 10:20:27 +0200

    Update deps

--- projects/ngx-scandium/src/lib/utility/translateConfig.ts
+++ projects/ngx-scandium/src/lib/utility/translateConfig.ts
@@ -1,32 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { TranslateLoader, TranslationObject } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { Observable } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 const path = `./assets/i18n/`;
 
-export class CoreTranslateLoader implements TranslateLoader {
-  constructor(private http: HttpClient) { }
-
-  getTranslation(lang: string): Observable<TranslationObject> {
-    return this.http.get('.' + path + lang + '.json').pipe(
-      map((langFile) => langFile),
-      catchError((err) => {
-        console.warn(err);
-        throw Error('Missing lang: ' + lang);
-      })
-    );
-  }
-}
-
-export const createTranslateLoader =
-  (http: HttpClient): TranslateHttpLoader => new TranslateHttpLoader(http, path, '.json');
-
-export const translateConfig = {
-  loader: {
-    provide: TranslateLoader,
-    useClass: CoreTranslateLoader,
-    deps: [HttpClient],
-  },
-};
+export const translateLoader = provideTranslateHttpLoader({ prefix: path, suffix: '.json' });
